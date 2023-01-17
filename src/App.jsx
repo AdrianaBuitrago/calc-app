63ce0ad7db877ca1dddbc02f72faf84fc7aee29a
Adriana Buitrago <buitragoadr@gmail.com>
2023-01-17 18:07:00 +0100

    Limpieza inicio del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+//importación
+import React from 'react'
+import './App.css'
+
+//Arrow Function
+const App = () => {
+    //Lo que ejecuta la función
+    return (
+        <main className="react-calculator">
+            <div className="result"></div>
+            <div className="numbers">
+                <button>1</button>
+                <button>2</button>
+                <button>3</button>
+                <button>4</button>
+                <button>5</button>
+                <button>6</button>
+                <button>7</button>
+                <button>6</button>
+                <button>9</button>
+                <button>0</button>
+            </div>
+            <div className="functions">
+                <button>clear</button>
+                <button>r</button>
+            </div>
+            <div className="math-operations">
+                <button></button>
+                <button></button>
+                <button></button>
+                <button></button>
+                <button></button>
+            </div>
+            <div className="numbers">
+                <button></button>
+                <button></button>
+                <button></button>
+                <button></button>
+                <button></button>
+            </div>
+        </main>
+    )
+}
+
+export default App
+//exportación
